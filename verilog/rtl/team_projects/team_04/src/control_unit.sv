e1dd238cf87a2d2fc5d1a0c53b19e52899cb0984
Darren Y Huang <huan2137@ecelnx33.ecn.purdue.edu>
2025-07-08 16:43:14 -0400

    request unit tb wip

--- verilog/rtl/team_projects/team_04/src/control_unit.sv
+++ verilog/rtl/team_projects/team_04/src/control_unit.sv
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> a71e555e5c7f0c655ef566e9ea7adba670e17709
 module control_unit (
   input logic BranchConditionFlag,
   input logic [31:0] instruction,
--- verilog/rtl/team_projects/team_04/testbench/request_unit_tb.sv
+++ verilog/rtl/team_projects/team_04/testbench/request_unit_tb.sv
@@ -1,17 +1,60 @@
 `timescale 1ns / 1ps
 module request_unit_tb;
-logic clk, rst, i_ack, d_ack, freeze;
-logic [31:0] PC, mem_address, stored_data, i_address, d_address, mem_store;
 
-request_unit r1(.clk(clk),.rst(rst),.i_ack(i_ack),.d_ack(d_ack),.freeze(freeze),.PC(PC),.mem_address(mem_address),.stored_data(stored_data),
-.i_address(i_address),.d_address,.mem_store(mem_store));
+  logic clk, rst, i_ack, d_ack, freeze;
+  logic [31:0] PC, mem_address, stored_data, i_address, d_address, mem_store;
 
-always #5 clk = ~clk; 
+  request_unit r1(
+    .clk(clk),
+    .rst(rst),
+    .i_ack(i_ack),
+    .d_ack(d_ack),
+    .freeze(freeze),
+    .PC(PC),
+    .mem_address(mem_address),
+    .stored_data(stored_data),
+    .i_address(i_address),
+    .d_address(),
+    .mem_store(mem_store));
 
+  always begin
+    #1
+    clk = ~clk;
+  end
 
+  task reset();
+    rst = 1; #1;
+    rst = 0; #1;
+  endtask
 
+  task compare(int expected, int actual); 
+  begin
+    if (expected == actual) begin
+      $display("PASSED test case (expected = actual = %d)", expected);
+    end else begin
+      $display("FAILED test case \n expected = %d, actual = %d", expected, actual);
+    end
+  end
+  endtask
 
+  initial begin
+    // dump signals to see them in waveform
+    $dumpfile("waves/request_unit.vcd");
+    $dumpvars(0, request_unit_tb);
 
+    // initialize signals
+    clk = 0;
+    rst = 0;
+    MemRead = 0;
+    MemWrite = 0;
+    i_ack = 1;
+    d_ack = 1;
+    
+    
 
+    // finish the simulation
+    #1 
+    $finish;
+  end
 
 endmodule
