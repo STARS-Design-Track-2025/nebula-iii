709599932957cd39382960b2c120375e185e897b
Eileen Koh <kohe@ecelnx08.ecn.purdue.edu>
2025-07-11 15:44:45 -0400

    added comments formatting and edited logic names
    "

--- verilog/rtl/team_projects/team_08/src/t08_CPU.sv
+++ verilog/rtl/team_projects/team_08/src/t08_CPU.sv
@@ -3,50 +3,51 @@ The top level module for the CPU (brings the parts of the CPU together)
 */
 
 module t08_CPU (
-    input logic clk, nRst, //Clock and active-low reset. 
-    input logic [31:0] data_in, //Data in from memory to memory handler.
-    input logic [31:0] instruction, //Instruction that the CPU receives, given to control unit decoder. 
-    output logic [31:0] data_out, //Data outputted to memory from memory handler. (Outputted to MMIO)
-    output logic [31:0] mem_address, //Indicates an address in memory (Outputted to MMIO)
-    output logic read_out, write_out //Read and write enable outputs (Outputted to MMIO)
+    input logic clk, nRst,                  //Clock and active-low reset. 
+    input logic [31:0] data_in,             //memory to memory handler: data in
+    input logic [31:0] instruction,         //memory to CPU: instruction 
+    output logic [31:0] data_out,           //memory handler to mmio: data outputted 
+    output logic [31:0] mem_address,        //memory handler to mmio: address in memory
+    output logic read_out, write_out        //memory handler to mmio: read and write enable
 );
 
-    logic [31:0] program_counter; //Program counter.
-    logic jump, branch; //Jump and branch signals.
+    logic [31:0] program_counter;                               //Program counter
+    logic [31:0] return_address;                                //fetch to registers: return address to be stored 
 
-    logic [31:0] immediate; //Immediate value outputted from control unit. 
-    logic [2:0] func3; //Function 3. 
+    logic [2:0] func3;                                          //CU to memory handler: function 3. 
+    logic mem_en_read, mem_en_write;                            //CU to memory handler: Read and write enable signals 
+    logic reg_en_read_1, reg_en_read_2, reg_en_write;           //CU to registers: enable signals 
+    logic [4:0] reg1, reg2, regd; //CU to registers: address inputs 
+    logic [1:0] data_in_control;                                //CU to registers: select line for the input mux                            //CU to fetch
+    logic [31:0] immediate;                                     //CU to ALU: immediate value  
+    logic [5:0] alu_control;                                    //CU to ALU: operation select    
+    logic jump;                                                 //CU to fetch: jump signal
+    
+    logic [31:0] mem_to_reg;                                    //memory hander to registers: data 
 
-    logic [31:0] return_address; //Return address to be stored in registers. 
+    logic [31:0] reg_out_1;                                     //registers to ALU and memory hander 
+    logic [31:0] reg_out_2;                                        //registers to ALU 
+    
+    logic [31:0] alu_data_out;                                  //ALU to registers and memory handler: ALU output
+    logic branch;                                               //ALU to fetch: branch signal
 
-    logic mem_en_read, mem_en_write; //Read and write enable signals for memory handler. 
-    logic [31:0] mem_to_reg; //Data output from memory handler to registers. 
+    t08_fetch fetch(.imm_address(immediate), .clk(clk), .nrst(nRst), .jump(jump), .branch(branch), 
+    .program_counter(program_counter), .ret_address(return_address));
 
-    logic reg_en_read_1, reg_en_read_2, reg_en_write; //Enable signals for the registers. 
-    logic [4:0] reg_address_r1, reg_address_r2, reg_address_rd; //Address inputs for the registers. 
-    logic [1:0] reg_data_in_control; //Select line for the input multiplexer for the registers
-    logic [31:0] reg_out_1, reg_out_2; //Outputs from the registers. 
+    t08_handler handler(.rs1(reg_out_1), .mem(data_in), .mem_address(mem_address), .write(mem_en_write), 
+        .read(mem_en_read), .clk(clk), .nrst(nRst), .func3(func3), .data_reg(mem_to_reg), .data_mem(data_out), 
+        .addressnew(), .writeout(write_out), .readout(read_out));
 
-    logic [5:0] alu_control; //Operation select for the alu. 
-    logic [31:0] alu_data_out; //Result outputted from the alu. 
+    t08_control_unit control_unit(.reset(nRst), .instruction(instruction), .read(mem_en_read), .write(mem_en_write), 
+        .funct3(func3), .data_in_control(data_in_control), .reg1(reg1), .reg2(reg2), .regd(regd), .en_read_1(reg_en_read_1), 
+        .en_read_2(reg_en_read_2), .en_write(reg_en_write), .immediate(immediate), .alu_control(alu_control), .jump(jump));
 
-    t08_fetch fetch(.imm_address(immediate), .clk(clk), .nrst(nRst), .jump(jump), .branch(branch), .program_counter(program_counter), 
-        .ret_address(return_address));
+    t08_alu alu(.clk(clk), .nRst(nRst), .reg1(reg_out_1), .reg2(reg_out_2), .immediate(immediate), 
+        .program_counter(program_counter), .alu_control(alu_control), .data_out(alu_data_out), .branch(branch));
 
-    t08_handler handler(.rs1(reg_out_1), .mem(data_in), .mem_address(mem_address), .write(mem_en_write), .read(mem_en_read), 
-        .clk(clk), .nrst(nRst), .func3(func3), .data_reg(mem_to_reg), .data_mem(data_out), .addressnew(), 
-        .writeout(write_out), .readout(read_out));
-
-    t08_control_unit control_unit(.reset(nRst), .instruction(instruction), .read(mem_en_read), .write(mem_en_write), .funct3(func3), 
-        .data_in_control(reg_data_in_control), .reg1(reg_address_r1), .reg2(reg_address_r2), .regd(reg_address_rd), 
-        .en_read_1(reg_en_read_1), .en_read_2(reg_en_read_2), .en_write(reg_en_write), .immediate(immediate), .alu_control(alu_control), .jump(jump));
-
-    t08_alu alu(.clk(clk), .nRst(nRst), .reg1(reg_out_1), .reg2(reg_out_2), .immediate(immediate), .program_counter(program_counter), 
-        .alu_control(alu_control), .data_out(alu_data_out), .branch(branch));
-
-    t08_registers registers(.clk(clk), .nRst(nRst), .address_r1(reg_address_r1), .address_r2(reg_address_r2), .address_rd(reg_address_rd), 
-        .data_in_frommemory(mem_to_reg), .data_in_frominstructionfetch(return_address), .data_in_fromalu(alu_data_out), 
-        .data_in_control(reg_data_in_control), .en_read_1(reg_en_read_1), .en_read_2(reg_en_read_2), .en_write(reg_en_write), 
-        .data_out_r1(reg_out_1), .data_out_r2(reg_out_2));
+    t08_registers registers(.clk(clk), .nRst(nRst), .address_r1(reg1), .address_r2(reg2), .address_rd(regd), 
+        .data_in_frommemory(mem_to_reg), .data_in_frominstructionfetch(return_address), 
+        .data_in_fromalu(alu_data_out), .data_in_control(data_in_control), .en_read_1(reg_en_read_1), 
+        .en_read_2(reg_en_read_2), .en_write(reg_en_write), .data_out_r1(reg_out_1), .data_out_r2(reg_out_2));
 
 endmodule
