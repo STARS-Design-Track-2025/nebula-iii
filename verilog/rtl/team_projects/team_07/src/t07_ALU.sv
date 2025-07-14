875412bda7dfc3ce63cc3de5f6e7636cb4c1457b
Kameswari Mantha <kmantha@ecelnx11.ecn.purdue.edu>
2025-07-14 15:07:52 -0400

    alu flag changes

--- verilog/rtl/team_projects/team_07/src/t07_ALU.sv
+++ verilog/rtl/team_projects/team_07/src/t07_ALU.sv
@@ -1,6 +1,6 @@
 module t07_ALU (
     input logic [31:0] valA, valB,
-    input logic [2:0] func3,
+    input logic [3:0] ALUOp,
     output logic [31:0] result,
     output logic [5:0] ALUflags
 );
@@ -15,32 +15,32 @@ module t07_ALU (
 
     //choose operation- Page 51 RVALP
     always_comb begin
-        case (func3)
-            3'd0: result = valA + valB;
-            3'd1: result = valA & valB;
-            3'd2: result = valA | valB;
-            3'd3: result = valA << valB[4:0];
-            3'd4: if(valA < valB) begin result = 1; end else begin result = 0; end 
-            3'd5: if(convert(valA) < convert(valB)) begin result = 1; end else begin result = 0; end
-            3'd6: if (valA < valB) begin result = 1; end else begin result = 0; end
-            3'd7: result = valA >>> valB[4:0];
-            3'd8: result = valA >> valB[4:0];
-            3'd9: result = valA - valB;
-            3'd10: result = valA ^ valB;
+        case (ALUOp)
+            4'd0: result = valA + valB; //add 
+            4'd1: result = valA & valB; //and
+            4'd2: result = valA | valB; //or
+            4'd3: result = valA << valB[4:0]; //sll
+            4'd4: if(valA < valB) begin result = 1; end else begin result = 0; end //slt
+            4'd5: if(convert(valA) < convert(valB)) begin result = 1; end else begin result = 0; end //sltu
+            4'd6: result = valA >>> valB[4:0]; //sra
+            4'd7: result = valA >> valB[4:0]; //srl
+            4'd8: result = valA - valB; //sub
+            4'd9: result = valA ^ valB; //xor
             default: result = 32'b0;
         endcase
     end
 
     //flag logic- Page 58 RVALP
-    assign ALUflags = 6'd0;
+    assign ALUflags = 7'd0;
 
     always_comb begin
-        if (result == 32'b0) begin ALUflags[0] = 1; end
-        if (valA >= valB) begin ALUflags[1] = 1; end
-        if (valA[30:0] >= valB[30:0]) begin ALUflags[2] = 1; end
-        if (valA < valB) begin ALUflags[3] = 1; end
-        if (valA[30:0] < valB[30:0]) begin ALUflags[4] = 1; end
-        if (valA != valB) begin ALUflags[5] = 1; end     
+        if (result == 32'b0) begin ALUflags[0] = 1; end //zeroFlag
+        if (valA >= valB) begin ALUflags[1] = 1; end //greater than or equal 
+        if (convert(valA) >= convert(valB)) begin ALUflags[2] = 1; end //greater than or equal unsigned
+        if (valA < valB) begin ALUflags[3] = 1; end //less than
+        if (convert(valA) < convert(valB)) begin ALUflags[4] = 1; end //less than unsigned
+        if (valA != valB) begin ALUflags[5] = 1; end //not equal
+        if (valA == valB) begin ALUflags[6] = 1; end //equal
     end
 
 endmodule
