b167055a9d4d1aa2052453ab06dbd8c35c119bae
Eileen Koh <kohe@purdue.edu>
2025-07-15 10:06:42 -0400

    changes to mmio

--- verilog/rtl/team_projects/team_08/src/t08_mmio.sv
+++ verilog/rtl/team_projects/team_08/src/t08_mmio.sv
@@ -5,29 +5,80 @@
 //interface with wishbone
 module t08_mmio (
     //from memory handler
-    input read,
-    input write,
-    input [31:0] address,
-    input [31:0] mh_data,
+    input logic read,                   //command to read, source specified by address
+    input logic write,                  //command to write, destination specified by address
+    input logic [31:0] address,         //location to read from or write to
+    input logic [31:0] mh_data,         //data to write
     //from I2C
-    input [7:0] xh,         
-    input [7:0] xl,         
-    input [7:0] yh,
-    input [7:0] yl,
-    input done,
+    input logic [7:0] xh,               //
+    input logic [7:0] xl,         
+    input logic [7:0] yh,
+    input logic [7:0] yl,
+    input logic done,
     //from Memory: data
-    input [31:0] mem_data,
+    input logic [31:0] mem_data,
     //to memory handler
-    output [31:0] mh_data_o,
+    output logic [31:0] mh_data_o,      //data read
+    output logic busy,
     //to SPI
-    output [31:0] parameters,
-    output [7:0] command,
-    output readwrite,
-    output enable 
+    output logic [31:0] parameters,
+    output logic [7:0] command,
+    output logic readwrite,
+    output logic enable 
 );
 
+typedef enum logic[1:0] {
+    IDLE,
+    WRITE,
+    READ
+ } state;
+
+state curr_state, next_state;
+logic next_busy;
+
+always_ff@(posedge clk, posedge reset) begin
+    if (reset) begin
+        curr_state <= IDLE;
+    end else if (done) begin
+        curr_State <= next_state;
+        busy <= next_busy;
+    end
+end
+
 always_comb begin
-    if
+   case(curr_state)
+        IDLE: begin
+            if(WRITE_I && !READ_I) begin
+                next_BUSY_O = 1'b1;
+                next_state  = WRITE;
+            end
+            if(!WRITE_I && READ_I) begin
+                next_BUSY_O = 1'b1;
+                next_state  = READ;
+            end
+        end     
+        WRITE: begin
+            next_ADR_O  = ADR_I;
+            next_DAT_O  = CPU_DAT_I;
+            next_SEL_O  = SEL_I;
+            next_WE_O   = 1'b1;
+            next_STB_O  = 1'b1;
+            next_CYC_O  = 1'b1;
+            next_BUSY_O = 1'b1;
+
+            if(ACK_I) begin
+                next_state = IDLE;
+
+                next_ADR_O  = '0;
+                next_DAT_O  = '0;
+                next_SEL_O  = '0;
+                next_WE_O   = '0;
+                next_STB_O  = '0;
+                next_CYC_O  = '0;
+                next_BUSY_O = '0;
+            end
+        end
+        READ: begin 
 end
 
 
