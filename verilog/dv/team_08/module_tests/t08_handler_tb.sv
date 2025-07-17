b995537404b4d3c18571fb864dde4d301a4b35aa
aharlano <aharlano@purdue.edu>
2025-07-17 10:02:59 -0400

    Update t08_handler_tb.sv

--- verilog/dv/team_08/module_tests/t08_handler_tb.sv
+++ verilog/dv/team_08/module_tests/t08_handler_tb.sv
@@ -15,7 +15,7 @@ always #3 done = ~done;
 t08_handler blockhandle(.counter(counter), .instruction(instruction), .busy(busy), .done(done), .readout(readout), .writeout(writeout), .fromregister(fromregister), .frommem(frommem), .mem_address(mem_address), .write(write), .read(read), .clk(clk),.nrst(nrst), .func3(func3), .toreg(toreg), . tomem(tomem), .addressnew(addressnew));
 
 initial begin
-    $dumpfile("waves/t08_handler.vcd"); 
+    $dumpfile("t08_handler.vcd"); 
     $dumpvars(0, t08_handler_tb);
     
     tfr; #1;
