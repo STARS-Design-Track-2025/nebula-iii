1c92d887df32bf5d9e3edaef44e318e077c1b4e4
Owen Yao <oyao@ecelnx30.ecn.purdue.edu>
2025-07-17 14:34:51 -0400

    fixed counter

--- verilog/rtl/team_projects/team_04/src/screenCounter.sv
+++ verilog/rtl/team_projects/team_04/src/screenCounter.sv
@@ -1,28 +1,33 @@
 module screenCounter (
-    input logic clk, rst, enableEdge,
-    output logic [22:0] ct
+  input logic clk, rst, enableEdge,
+  output logic [22:0] ct
 );
 
-logic [22:0] nextCt, currentCt;
+  logic [22:0] nextCt, currentCt;
 
-always_comb begin
-    if (enableEdge) begin
-        nextCt = 0;
-    end else if (currentCt == 23'b11111111111111111111111) begin
-        nextCt = currentCt;
-    end else begin
-        nextCt = currentCt + 1;
+  always_comb begin
+    if (enableEdge && currentCt == 0) begin
+      nextCt = 1;
+    end 
+    else if (enableEdge) begin
+      nextCt = 0;
     end
-end
+    else if (currentCt == 23'b11111111111111111111111) begin
+      nextCt = currentCt;
+    end 
+    else begin
+      nextCt = currentCt + 1;
+    end
+  end
 
-always_ff @(posedge clk, posedge rst) begin
+  always_ff @(posedge clk, posedge rst) begin
     if (rst) begin
-        currentCt <= 0;
+      currentCt <= 0;
     end else begin
-        currentCt <= nextCt;
+      currentCt <= nextCt;
     end
-end
+  end
 
-assign ct = currentCt;
+  assign ct = currentCt;
 
 endmodule
