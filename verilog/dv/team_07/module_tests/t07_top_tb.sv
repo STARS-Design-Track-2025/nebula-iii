45db2a022b5f9c4d4b2aefdfc0b06707278dd09b
annadalton <annazd06@gmail.com>
2025-07-21 13:58:22 -0400

    merge

---
+++ verilog/dv/team_07/module_tests/t07_top_tb.sv
@@ -0,0 +1,32 @@
+`timescale 1ms / 1ps
+module t07_top_tb();
+    logic clk, nrst;
+    logic FPUFlag, invalError;
+
+    t07_top top0(.clk(clk), .nrst(nrst), .FPUFlag(FPUFlag), .invalError(invalError));
+
+    task reset(); begin
+        #1
+        nrst = ~nrst;        
+        #1
+        nrst = ~nrst;
+    end
+    endtask
+
+    always begin
+        #2
+        clk = ~clk;
+    end
+
+    initial begin
+        $dumpfile("t07_CPU.vcd");
+        $dumpvars(0, t07_CPU_tb);
+        clk = 0;
+        nrst = 1;
+        reset();
+
+        #20
+        $finish;
+    end
+
+endmodule
