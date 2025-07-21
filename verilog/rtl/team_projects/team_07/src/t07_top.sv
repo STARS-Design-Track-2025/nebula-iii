a644dfb53744a32b7e6a752f6065fd6690ff4bcb
annadalton <annazd06@gmail.com>
2025-07-21 11:55:45 -0400

    finished wishbone instantiation

--- verilog/rtl/team_projects/team_07/src/t07_top.sv
+++ verilog/rtl/team_projects/team_07/src/t07_top.sv
@@ -30,7 +30,9 @@ logic [1:0] rwi_in; //read = 10, write = 01, idle = 00
 logic [31:0] addrToSRAM, dataToSRAM; //addr_out in MMIO 
 
 //wishbone manager output to wishbone arbitrator
-logic [31:0] addrToAr, dataToAr;
+logic [31:0] addrWMToAr, dataWMToAr;
+logic [3:0] selToAr;
+logic weToAr, stbToAr, cycToAr;
 
 //wishbone arbitrator output to wishbone decoder
 logic cycToDec, stbToDec, weToDec;
@@ -38,17 +40,33 @@ logic [31:0] addrToDec, dataToDec;
 logic [3:0] selToDec;
 
 //wishbone arbitrator output to wishbone manger
-logic [31:0] dataToWM;
+logic [31:0] ackToWM; dataArToWM;
+
+//wishbone decoder output to wishbone arbitrator
+logic ackToAr;
+logic [31:0] dataDecToAr;
 
 //wishbone manager output to user design
 logic [31:0] dataToMMIO;
 logic busyToMMIO;
 
+//outputs of WB decoder
+logic [6 + 3:0] cyc_out;
+logic [6 + 3:0] stb_out;
+logic [6 + 3:0] we_out;
+logic [(32 * (10)) - 1:0] addr_out;
+logic [(32 * (10)) - 1:0] data_out;
+logic [(4 * (10)) - 1:0] sel_out;
+
+//input SRAM to decoder
+logic [6 + 3:0] ackDec_in;
+logic [(32 * (10)) - 1:0] dataDec_in;
+
 t07_CPU CPU(.busy(busyFromCPU), .externalMemAddr(exMemAddr_CPU), .exMemData_out(exMemData_CPU), .exInst(instr), .memData_in(memData_in), .rwi(rwi), .FPUFlag(FPUFlag), .invalError(invalError), .clk(clk), .nrst(nrst));
-t07_MMIO MMIO(.addr_in(exMemAddr_CPU), .memData_in(exMemData_CPU), .rwi_in(rwi_in), .inst(instr), .ExtData_in(exMemData_CPU), .regData_in(), .ack_REG(), .ack_TFT(), .ri_out(), .addr_outREG(), .ExtData_out(), .busy(), .writeInstruction_out(), .writeData_outTFT(), .wi_out(), .addr_outTFT(), .rwi_out(), .addr_out(addrToSRAM), .writeData_out(dataToSRAM), .busy_o());
-wishbone_manager wishbone0(.nRST(nrst), .CLK(clk), .DAT_I(), .ACK_I(), .CPU_DATA_I(dataToSRAM /*check*/), .ADR_I(addrToSRAM), .SEL_I('1), .WRITE_I(write), .READ_I(read), .ADR_O(), .DAT_O(addrToDec), .SEL_O(), .WE_O(), .STB_O(), .CYC_O(), .CPU_DAT_O(), .BUSY_O(busy));
+t07_MMIO MMIO(.addr_in(exMemAddr_CPU), .memData_in(exMemData_CPU), .rwi_in(rwi_in), .inst(instr), .ExtData_in(dataToMMIO), .regData_in(), .ack_REG(), .ack_TFT(), .ri_out(), .addr_outREG(), .ExtData_out(), .busy(), .writeInstruction_out(), .writeData_outTFT(), .wi_out(), .addr_outTFT(), .rwi_out(), .addr_out(addrToSRAM), .writeData_out(dataToSRAM), .busy_o());
+wishbone_manager wishbone0(.nRST(nrst), .CLK(clk), .DAT_I(dataArToWM), .ACK_I(ackToWM), .CPU_DATA_I(dataToSRAM), .ADR_I(addrToSRAM), .SEL_I('1), .WRITE_I(write), .READ_I(read), .ADR_O(addrWMToAr), .DAT_O(dataWMToAr), .SEL_O(selToAr), .WE_O(weToAr), .STB_O(stbToAr), .CYC_O(cycToAr), .CPU_DAT_O(dataToMMIO), .BUSY_O(busy));
 //SRAM
-wishbone_arbitrator wishboneA0(.CLK(clk), .nRST(nrst), .A_ADR_I(), .A_DAT_I(), .A_SEL_I(), .A_WE_I(), .A_STB_I(), .A_CYC_I(), .A_DAT_O(), .A_ACK_O(), .DAT_I(), .ACK_I(), .ADR_O(addrToDec), .DAT_O(dataToDec), .SEL_O(selToDec), .WE_O(weToDec), .STB_O(stbToDec), .CYC_O(cycToDec));
-wishbone_decoder wishboneD0(.CLK(clk), .nRST(nrst), .wbs_ack_i_peroph(), .wbs_dat_i_peroph(), .wbs_ack_o_m(), .wbs_dat_o_m(), .wbs_cyc_i_m(cycToDec), .wbs_stb_i_m(stbToDec), .wbs_we_i_m(weToDec), .wbs_adr_i_m(addrToDec), .wbs_dat_i_m(dataToDec), .wbs_sel_i_m(selToDec), .wbs_cyc_o_periph(), .wbs_stb_o_periph(), .wbs_we_o_periph(), .wbs_adr_o_periph(), .wbs_dat_o_periph(), .wbs_sel_o_periph());
+wishbone_arbitrator wishboneA0(.CLK(clk), .nRST(nrst), .A_ADR_I(addrWMToAr), .A_DAT_I(dataWMToAr), .A_SEL_I(selToAr), .A_WE_I(weToAr), .A_STB_I(stbToAr), .A_CYC_I(cycToAr), .A_DAT_O(dataArToWM), .A_ACK_O(ackToWM), .DAT_I(dataDecToAr), .ACK_I(ackToAr), .ADR_O(addrToDec), .DAT_O(dataToDec), .SEL_O(selToDec), .WE_O(weToDec), .STB_O(stbToDec), .CYC_O(cycToDec));
+wishbone_decoder wishboneD0(.CLK(clk), .nRST(nrst), .wbs_ack_i_peroph(ackDec_in), .wbs_dat_i_peroph(dataDec_in), .wbs_ack_o_m(ackToAr), .wbs_dat_o_m(dataDecToAr), .wbs_cyc_i_m(cycToDec), .wbs_stb_i_m(stbToDec), .wbs_we_i_m(weToDec), .wbs_adr_i_m(addrToDec), .wbs_dat_i_m(dataToDec), .wbs_sel_i_m(selToDec), .wbs_cyc_o_periph(cyc_out), .wbs_stb_o_periph(stb_out), .wbs_we_o_periph(we_out), .wbs_adr_o_periph(addr_out), .wbs_dat_o_periph(data_out), .wbs_sel_o_periph(sel_out));
 
 endmodule
