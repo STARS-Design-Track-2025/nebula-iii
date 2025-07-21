a86432c6d136fd97caf130286c06fc2da6d1f057
rfreedma <rfreedma@purdue.edu>
2025-07-21 16:56:47 -0400

    fetch with busy

--- verilog/rtl/team_projects/team_07/src/t07_fetch.sv
+++ verilog/rtl/team_projects/team_07/src/t07_fetch.sv
@@ -1,7 +1,7 @@
 // module to fetch instructions and manage the program counter
 module t07_fetch (
     // Inputs
-    input logic clk, nrst,
+    input logic clk, nrst, busy_o,
     input logic [31:0] ExtInstruction, // External instruction input
     input logic [31:0] programCounter, // Current program counter value
     
@@ -13,7 +13,7 @@ module t07_fetch (
 
 logic [31:0] n_ExtInstruction, n_PC_out; // Next instruction to fetch
 
-always_ff @( negedge nrst, posedge clk) begin
+always_ff @( negedge busy_o, negedge nrst, posedge clk) begin
     if (~nrst) begin
         Instruction <= 32'b0; // Reset instruction to zero on reset
         PC_out <= 32'b0; // Reset program counter output to zero
