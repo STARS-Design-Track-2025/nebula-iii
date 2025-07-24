49d5f4ef828dd0aee6978da1875e477970b18c8f
Kameswari Mantha <kmantha@ecelnx11.ecn.purdue.edu>
2025-07-24 13:28:55 -0400

    modified overflow logic in ALU

--- verilog/rtl/team_projects/team_07/src/t07_ALU.sv
+++ verilog/rtl/team_projects/team_07/src/t07_ALU.sv
@@ -4,16 +4,10 @@ module t07_ALU (
     output logic [31:0] result,
     output logic [6:0] ALUflags
 );
-<<<<<<< HEAD
 //choose operation- Page 51 RVALP
-=======
-
-
-    //choose operation- Page 51 RVALP
->>>>>>> 29944fb9d154a2b58919c355db2bf54c5639a7b9
     always_comb begin
         case (ALUOp)
-            4'd0: result = valA + valB; //add 
+            4'd0: result = valA + valB; //add, has overflow 
             4'd1: result = valA & valB; //and
             4'd2: result = valA | valB; //or
             4'd3: result = valA << valB[4:0]; //sll
@@ -21,28 +15,27 @@ module t07_ALU (
             4'd5: if($unsigned(valA) < $unsigned(valB)) begin result = 1; end else begin result = 0; end //sltu
             4'd6: result = valA >>> valB[4:0]; //sra
             4'd7: result = valA >> valB[4:0]; //srl
-            4'd8: result = valA - valB; //sub
+            4'd8: result = valA - valB; //sub, has overflow
             4'd9: result = valA ^ valB; //xor
             default: result = 32'b0;
         endcase
 
 
         //overflow
-        if ($size(result) > 31) begin
-            result = 32'b0;
+        if (ALUOp == 4'd0) begin
+            if(((valA > $signed(32'b0)) && (valB > $signed(32'b0)) && ($signed(result) < $signed(32'b0))) || ((valA < $signed(32'b0)) && (valB < $signed(32'b0)) && ($signed(result) > $signed(32'b0)))) begin
+                result = 32'b0;
+            end
+        end else if (ALUOp == 4'd8) begin
+            if(((valA > $signed(32'b0)) && (valB < $signed(32'b0)) && ($signed(result) < $signed(32'b0))) || ((valA < $signed(32'b0)) && (valB > $signed(32'b0)) && ($signed(result) > $signed(32'b0)))) begin
+                result = 32'b0;
+            end
         end
     end
 
     //flag logic- Page 58 RVALP
-<<<<<<< HEAD
     always_comb begin
         ALUflags = 7'd0;
-=======
-
-    always_comb begin
-        ALUflags = 7'd0;
-
->>>>>>> 29944fb9d154a2b58919c355db2bf54c5639a7b9
         if (result == 32'b0) begin ALUflags[0] = 1; end //zeroFlag
         if (valA >= valB) begin ALUflags[1] = 1; end //greater than or equal 
         if ($unsigned(valA) >= $unsigned(valB)) begin ALUflags[2] = 1; end //greater than or equal unsigned
