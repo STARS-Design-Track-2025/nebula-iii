cb33ab23b1f5bed379f355771776a3d8eb5f5b05
Kameswari Mantha <kmantha@purdue.edu>
2025-07-31 15:29:07 -0400

    updated counter logic

--- verilog/rtl/team_projects/team_07/src/t07_spitft.sv
+++ verilog/rtl/team_projects/team_07/src/t07_spitft.sv
@@ -16,15 +16,18 @@ module t07_spitft (
 logic [63:0] dataforOutput, next_data;
 logic state, next_state;
 logic printState;
+logic [6:0] counter, next_ctr;
 
 
 always_ff @(negedge nrst, posedge clk) begin
     if(~nrst) begin
         dataforOutput <= '0;
+        counter <= 7'b0;
         state <= 1'b0;
     end else begin
         dataforOutput <= next_data;
         state <= next_state;
+        counter <= next_ctr;
     end
 end
 
@@ -35,17 +38,19 @@ always_comb begin
     ack = 0;
     chipSelect = 1;
     sclk = 0;
+    next_ctr = counter;
 
     case (state) 
-      1'b0: begin if (wi == 1) begin chipSelect = 0; ack = 1; sclk = clk; next_data = {address[31:24], data[31:24],  address[23:16], data[23:16], address[15:8], data[15:8], address[7:0], data[7:0]}; next_state = 1'b1; end else begin next_state = 1'b0; end end //load 64 bits
-      1'b1: begin if (wi == 1 && dataforOutput != '0) begin
+      1'b0: begin if (wi == 1) begin next_ctr = 7'b0; chipSelect = 0; ack = 1; sclk = clk; next_data = {address[31:24], data[31:24],  address[23:16], data[23:16], address[15:8], data[15:8], address[7:0], data[7:0]}; next_state = 1'b1; end else begin next_state = 1'b0; end end //load 64 bits
+      1'b1: begin if (wi == 1 && counter < 7'd64) begin
                 ack = 1;
                 chipSelect = 0; 
                 sclk = clk;
                 bitData = dataforOutput[63];
                 next_data = dataforOutput << 1;
                 next_state = 1'b1;
-        end else if (wi == 1 && dataforOutput == '0) begin
+                next_ctr = counter + 1;
+        end else if (wi == 1 && counter == 7'd64) begin
             ack = 0;
             chipSelect = 1;
             next_state = 1'b0;
