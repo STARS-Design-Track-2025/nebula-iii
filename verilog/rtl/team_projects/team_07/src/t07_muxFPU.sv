7d4709ad4b08d03ce58933d6f67d2066e01d9783
annadalton <annazd06@gmail.com>
2025-08-05 21:33:08 -0400

    removed value C from mux

--- verilog/rtl/team_projects/team_07/src/t07_muxFPU.sv
+++ verilog/rtl/team_projects/team_07/src/t07_muxFPU.sv
@@ -1,22 +1,16 @@
 module t07_muxFPU(
-    input logic [31:0] regValA_i, regValB_i, regValC_i,
-    input logic [31:0] fpuRegValA_i, fpuRegValB_i, fpuRegValC_i,
+    input logic [31:0] regValA_i, regValB_i, 
+    input logic [31:0] fpuRegValA_i, fpuRegValB_i, 
     input [4:0] FPUOp,
-    output [31:0] FPUValA_o, FPUValB_o, FPUValC_o
+    output [31:0] FPUValA_o, FPUValB_o
 );
     always_comb begin
         if(FPUOp == 5'd21 || FPUOp == 5'd22) begin //int to float
             FPUValA_o = regValA_i;
             FPUValB_o = regValB_i;
-            FPUValC_o = '0; //val C not needed
-        end else if (FPUOp == 5'd0 || FPUOp == 5'd1 || FPUOp == 5'd2 || FPUOp == 5'd3) begin
-            FPUValA_o = fpuRegValA_i;
-            FPUValB_o = fpuRegValB_i;
-            FPUValC_o = fpuRegValC_i;
         end else begin
             FPUValA_o = fpuRegValA_i;
             FPUValB_o = fpuRegValB_i;
-            FPUValC_o = '0; //val C not needed
         end
     end
 endmodule
