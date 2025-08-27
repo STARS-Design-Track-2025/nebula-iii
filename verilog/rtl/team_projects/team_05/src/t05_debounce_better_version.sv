e60e6cc0825f6d5e32cb65927d2a6ec494e27644
weyer7 <weyer7@purdue.edu>
2025-08-27 19:58:05 -0400

    Update t05_debounce_better_version.sv removed extra modules

--- verilog/rtl/team_projects/team_05/src/t05_debounce_better_version.sv
+++ verilog/rtl/team_projects/team_05/src/t05_debounce_better_version.sv
@@ -31,25 +31,3 @@ module t05_debounce_better_version(
         end
     end
 endmodule
-
-
-
-// Slow clock enable for debouncing button 
-//  FPGA4schools
-// module clock_enable(input Clk_100M,output slow_clk_en);
-//     reg [26:0]counter=0;
-//     always @(posedge Clk_100M)
-//     begin
-//        counter <= (counter>=249999)?0:counter+1;
-//     end
-
-//     assign slow_clk_en = (counter == 249999)?1'b1:1'b0;
-// endmodule
-
-// D-flip-flop with clock enable signal for debouncing module 
-// module my_dff_en(input DFF_CLOCK, clock_enable,D, output reg Q=0);
-//     always @ (posedge DFF_CLOCK) begin
-//   if(clock_enable==1) 
-//            Q <= D;
-//     end
-// endmodule 
